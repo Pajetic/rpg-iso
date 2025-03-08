025940ccf3af738a937ef9cdc19e42605af27968
pajetic <mssjimmy@gmail.com>
2025-03-07 21:19:03 -0500

    Simple click to move + camera follow + placeholder assets

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,24 @@ public class PlayerController : MonoBehaviour {
     [SerializeField] private Transform target;
     
     NavMeshAgent navMeshAgent;
+
+    private Ray lastRay;
     
     private void Start() {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     private void Update() {
-        if (navMeshAgent != null) {
-            navMeshAgent.destination = target.position;
+        if (Input.GetMouseButtonDown(0)) {
+            MoveToCursor();
+        }
+    }
+    
+    private void MoveToCursor() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit)) {
+            navMeshAgent.destination = hit.point;
         }
     }
 }
