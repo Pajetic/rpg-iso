7702bbcb2037c888c2e7c5920bea7906c755b040
pajetic <mssjimmy@gmail.com>
2025-03-11 22:39:54 -0400

    Separte player controller and movement component

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,38 +2,26 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour {
-    private const string MOVE_SPEED_STRING = "MoveSpeed";
     
-    [SerializeField] private Transform target;
-    
-    private NavMeshAgent navMeshAgent;
-    private Animator animator;
-
+    MovementController movementController;
     private Ray lastRay;
-    
-    private void Start() {
-        navMeshAgent = GetComponent<NavMeshAgent>();
-        animator = GetComponent<Animator>();
-    }
 
+    private void Start() {
+        movementController = GetComponent<MovementController>();
+    }
+    
     private void Update() {
         if (Input.GetMouseButton(0)) {
             MoveToCursor();
         }
-        UpdateAnimator();
     }
     
     private void MoveToCursor() {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         if (Physics.Raycast(ray, out RaycastHit hit)) {
-            navMeshAgent.destination = hit.point;
+            movementController.MoveTo(hit.point);
         }
     }
 
-    private void UpdateAnimator() {
-        Vector3 velocity = navMeshAgent.velocity;
-        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
-        animator.SetFloat(MOVE_SPEED_STRING, localVelocity.magnitude);
-    }
 }
