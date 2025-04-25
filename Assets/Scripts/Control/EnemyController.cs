5466050740d2034679a3097ba7e85e16f3b7c205
pajetic <mssjimmy@gmail.com>
2025-04-24 22:18:35 -0400

    Enemy guard behavior + gizmo draw aggro range

--- Assets/Scripts/Control/EnemyController.cs
+++ Assets/Scripts/Control/EnemyController.cs
@@ -1,20 +1,32 @@
 using RPGIso.Combat;
 using RPGIso.Core;
+using RPGIso.Movement;
 using UnityEngine;
 
 namespace RPGIso.Control {
     public class EnemyController : MonoBehaviour {
-        [SerializeField] private float chaseDistance = 3f;
+        [SerializeField] private float targetDetectionDistance = 3f;
+        [SerializeField] private float targetInvestigationTime = 3f;
         
         private CombatController combatController;
         private Health health;
+        private MovementController movementController;
+        private ActionScheduler actionScheduler;
         private GameObject player;
+
+        // TODO split out enemy behavior types
+        private Vector3 guardPosition;
+        private float timeSinceLastTarget = float.MaxValue;
 
         private void Start() {
             combatController = GetComponent<CombatController>();
             health = GetComponent<Health>();
+            movementController = GetComponent<MovementController>();
+            actionScheduler = GetComponent<ActionScheduler>();
             // TODO don't use tags
             player = GameObject.FindWithTag("Player");
+            
+            guardPosition = transform.position;
         }
         
         private void Update() {
@@ -23,14 +35,37 @@ namespace RPGIso.Control {
             }
             
             if (IsPlayerInRange() && combatController.CanAttack(player)) {
-                combatController.Attack(player);
+                HandleAttack();
+            } else if (timeSinceLastTarget < targetInvestigationTime) {
+                HandleInvestigateTarget();
             } else {
-                combatController.CancelAction();
+                HandleReturnToGuardPosition();
             }
+            timeSinceLastTarget += Time.deltaTime;
+        }
+
+        private void HandleAttack() {
+            timeSinceLastTarget = 0;
+            combatController.Attack(player);
+        }
+
+        private void HandleInvestigateTarget() {
+            actionScheduler.CancelCurrentAction();
+        }
+
+        private void HandleReturnToGuardPosition() {
+            combatController.CancelAction();
+            movementController.MoveTo(guardPosition);
         }
 
         private bool IsPlayerInRange() {
-            return Vector3.Distance(transform.position, player.transform.position) < chaseDistance;
+            return Vector3.Distance(transform.position, player.transform.position) < targetDetectionDistance;
+        }
+        
+        // Can use OnDrawGizmosSelected
+        private void OnDrawGizmos() {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, targetDetectionDistance);
         }
     }
 }
